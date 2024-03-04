ea820b38d54305ab6d57f07b656b1e51e1878911
AlexBeovides <abasbeovides@gmail.com>
2024-03-04 16:02:04 -0500

    added ag-grid example table

--- src/pages/Clients.tsx
+++ src/pages/Clients.tsx
@@ -1,7 +1,40 @@
+import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
+import  { useState } from 'react';
+import { ColDef } from 'ag-grid-community';
+
+interface RowData {
+  make: string;
+  model: string;
+  price: number;
+}
+
 export const Clients = () => {
-  return (
-    <>
-      <div style={{ height: "90vh" }}></div>
+  const [rowData] = useState<RowData[]>([
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxster', price: 72000 }
+]);
+const [colDefs, setColDefs] = useState<ColDef<RowData>[]>([
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' },
+]);
+
+return (
+  <>
+  <br></br>
+  <br></br>
+  <br></br>
+  <br></br>
+  <br></br>
+  <br></br>
+    <div className="ag-theme-quartz" style={{ height: 400, width: 600 }}>
+        <AgGridReact<RowData>
+            rowData={rowData}
+            columnDefs={colDefs} />
+    </div>
     </>
-  );
+);
 };
