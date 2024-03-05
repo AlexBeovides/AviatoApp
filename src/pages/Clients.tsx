93e1a2cb095e763179e763ba7326779a33084b4b
AlexBeovides <abasbeovides@gmail.com>
2024-03-04 22:52:55 -0500

    added update functionality to clients table

--- src/pages/Clients.tsx
+++ src/pages/Clients.tsx
@@ -2,7 +2,7 @@ import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
 import  { useEffect,useState } from 'react';
-import { ColDef } from 'ag-grid-community';
+import { ColDef,CellValueChangedEvent } from 'ag-grid-community';
 
 type Client = {
   id: number;
@@ -15,11 +15,24 @@ export const Clients = () => {
 
 const [rowData, setRowData] = useState<Client[]>([]);
 const [colDefs] = useState<ColDef[]>([
-  { field: 'id', headerName: 'ID' },
-  { field: 'name', headerName: 'Name' },
-  { field: 'nationality', headerName: 'Nationality' },
-  { field: 'clientType', headerName: 'Client Type' },
+  { field: 'id', headerName: 'ID', editable: false },
+  { field: 'name', headerName: 'Name', editable: true },
+  { field: 'nationality', headerName: 'Nationality', editable: true },
+  { field: 'clientType', headerName: 'Client Type', editable: true },
 ]);
+
+const onCellValueChanged = (params : CellValueChangedEvent) => {
+  const updatedClient = params.data;
+
+  fetch(`https://localhost:32770/api/Clients/${updatedClient.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updatedClient),
+  })
+    .catch(error => console.error('Error:', error));
+};
 
 useEffect(() => {
   fetch('https://localhost:32770/api/Clients')
@@ -35,7 +48,9 @@ return (
     <div className="ag-theme-quartz" style={{ height: 400, width: 800, marginLeft: '30px',marginBottom: '10vh' }}>
         <AgGridReact<Client>
             rowData={rowData}
-            columnDefs={colDefs} />
+            columnDefs={colDefs} 
+            onCellValueChanged={onCellValueChanged}
+          />
     </div>
   </>
 );
