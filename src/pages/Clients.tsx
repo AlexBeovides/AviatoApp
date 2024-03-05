1ea44a9095e3d426e45c3731005311e47ee89746
AlexBeovides <abasbeovides@gmail.com>
2024-03-04 22:01:46 -0500

    added real api fetched data to the table

--- src/pages/Clients.tsx
+++ src/pages/Clients.tsx
@@ -1,40 +1,42 @@
 import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
-import  { useState } from 'react';
+import  { useEffect,useState } from 'react';
 import { ColDef } from 'ag-grid-community';
 
-interface RowData {
-  make: string;
-  model: string;
-  price: number;
-}
+type Client = {
+  id: number;
+  name: string;
+  nationality: string;
+  clientType: string;
+};
 
 export const Clients = () => {
-  const [rowData] = useState<RowData[]>([
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 }
+
+const [rowData, setRowData] = useState<Client[]>([]);
+const [colDefs] = useState<ColDef[]>([
+  { field: 'id', headerName: 'ID' },
+  { field: 'name', headerName: 'Name' },
+  { field: 'nationality', headerName: 'Nationality' },
+  { field: 'clientType', headerName: 'Client Type' },
 ]);
-const [colDefs, setColDefs] = useState<ColDef<RowData>[]>([
-    { field: 'make' },
-    { field: 'model' },
-    { field: 'price' },
-]);
+
+useEffect(() => {
+  fetch('https://localhost:32770/api/Clients')
+    .then(response => response.json())
+    .then(data => setRowData(data))
+    .catch(error => console.error('Error:', error));
+}, []);
 
 return (
   <>
-  <br></br>
-  <br></br>
-  <br></br>
-  <br></br>
-  <br></br>
-  <br></br>
-    <div className="ag-theme-quartz" style={{ height: 400, width: 600 }}>
-        <AgGridReact<RowData>
+    <h2 style={{ marginLeft: '30px',marginTop: '15vh',marginBottom:'30px' }}>Clients Data:</h2>
+
+    <div className="ag-theme-quartz" style={{ height: 400, width: 800, marginLeft: '30px',marginBottom: '10vh' }}>
+        <AgGridReact<Client>
             rowData={rowData}
             columnDefs={colDefs} />
     </div>
-    </>
+  </>
 );
 };
