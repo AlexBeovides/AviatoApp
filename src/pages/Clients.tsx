3a990d8471def2ac8446e1cc5d6f37492d579440
AlexBeovides <abasbeovides@gmail.com>
2024-03-05 08:15:48 -0500

    added and styled form for adding new clients to the db

--- src/pages/Clients.tsx
+++ src/pages/Clients.tsx
@@ -17,6 +17,7 @@ export const Clients = () => {
   const [rowData, setRowData] = useState<Client[]>([]);
   const [selectedRows, setSelectedRows] = useState<Client[]>([]);
   const [gridApi, setGridApi] = useState<GridApi | null>(null);
+  const [newClient, setNewClient] = useState<Client>({ id: 0, name: '', nationality: '', clientType: '' });
 
   const colDefs: ColDef[] = [
     { field: 'id', headerName: 'ID', editable: false, checkboxSelection: true },
@@ -24,6 +25,26 @@ export const Clients = () => {
     { field: 'nationality', headerName: 'Nationality', editable: true , filter: true, floatingFilter: true },
     { field: 'clientType', headerName: 'Client Type', editable: true, filter: true, floatingFilter: true },
   ];
+
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewClient({ ...newClient, [event.target.name]: event.target.value });
+  };
+
+  const handleFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+  
+    fetch('https://localhost:32770/api/Clients', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newClient),
+    })
+    .then(response => response.json())
+    .then(data => setRowData([...rowData, data]))
+    .catch(error => console.error('Error:', error));
+  };
 
   const onGridReady = (params: GridReadyEvent) => {
     setGridApi(params.api);
@@ -72,16 +93,26 @@ useEffect(() => {
 return (
   <>
     <h2 className='page-header'>Clients Data:</h2>
-    <button className='delete-button' onClick={deleteSelectedRows}>Delete Selected Rows</button>
-    <div className="ag-theme-quartz" style={{ height: 400, width: 800, marginLeft: '30px',marginBottom: '10vh' }}>
-        <AgGridReact<Client>
+    <div className='section-container'>
+      <div className='table-container'>
+        <div className="ag-theme-quartz client-table" style={{ height: 400, width: 800 }}>
+          <AgGridReact<Client>
             rowData={rowData}
-            columnDefs={colDefs} 
-            onCellValueChanged={onCellValueChanged}
-            onGridReady={onGridReady}
-            onSelectionChanged={onSelectionChanged}
-            rowSelection="multiple"
-          />
+              columnDefs={colDefs} 
+              onCellValueChanged={onCellValueChanged}
+              onGridReady={onGridReady}
+              onSelectionChanged={onSelectionChanged}
+              rowSelection="multiple"
+            />
+        </div>
+      <button className='my-button delete-button' onClick={deleteSelectedRows}>Delete Selected Rows</button>
+      </div>
+      <form className="form-container" onSubmit={handleFormSubmit}>
+        <input type="text" name="name" value={newClient.name} onChange={handleInputChange} placeholder="Name" required />
+        <input type="text" name="nationality" value={newClient.nationality} onChange={handleInputChange} placeholder="Nationality" required />
+        <input type="text" name="clientType" value={newClient.clientType} onChange={handleInputChange} placeholder="Client Type" required />
+        <button className='my-button' type="submit">Add New Client</button>
+      </form>
     </div>
   </>
 );
