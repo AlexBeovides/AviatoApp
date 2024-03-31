aecab3f3c809426351a49daa12984cf08ca8eaad
arielgg46 <lenin46ariel@gmail.com>
2024-03-31 18:50:00 -0500

    Connected Facilities page to backend

--- src/pages/Facilities.tsx
+++ src/pages/Facilities.tsx
@@ -1,34 +1,96 @@
 import "../styles/Facilities.scss";
 import { FacilityCard } from "../components/FacilityCard";
+import { API_BASE_URL } from "../config";
+import { useEffect, useState, useRef } from "react";
 
-// Change for generic imports, fetch from database
-import Breadway from "../assets/images/Breadway.jpg";
-import AMXWorkshop from "../assets/images/AMX-Workshop.jpg";
-import Tascon from "../assets/images/Tascon.jpg";
-import ArtesaniaDominicana from "../assets/images/ArtesaniaDominicana.jpg";
-import CambioExchange from "../assets/images/Cambio.jpg";
-import Ryu from "../assets/images/Ryu.jpg";
-import Tagliatella from "../assets/images/Tagliatella.jpg";
+type Airport = {
+  id: number;
+  name: string;
+  address: string;
+  isDeleted: Boolean;
+};
 
+type Facility = {
+  id: number;
+  name: string;
+  address: string;
+  imgUrl: string;
+  FacilityTypeId: number;
+  isDeleted: Boolean;
+};
 
 export const Facilities = () => {
-  const hStyle = { color: '#FF385C' };
+  const [airportsData, setAirportsData] = useState<Airport[]>([]);
+  const [facilitiesData, setFacilitiesData] = useState<Facility[]>([]);
+  const selectRef = useRef<HTMLSelectElement | null>(null);
+  const [selectWidth, setSelectWidth] = useState("auto");
+
+  const getFacilities = (airportId: number) => {
+    fetch(`${API_BASE_URL}/Facilities?airportId=${airportId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Response:", data);
+        setFacilitiesData(data);
+      })
+      .catch((error) => console.error("Error:", error));
+  };
+
+  const handleSelectChange = () => {
+    if (selectRef.current) {
+      const select = selectRef.current;
+      if (select.selectedIndex !== -1) {
+        const selectedOption = select.options[select.selectedIndex];
+        if (selectedOption) {
+          if (selectedOption.value == "") return;
+          setSelectWidth(`${selectedOption.text.length - 2}ch`);
+          const facilities = getFacilities(
+            airportsData[select.selectedIndex - 1].id
+          );
+          console.log(facilities);
+        }
+      }
+    }
+  };
+
+  useEffect(() => {
+    handleSelectChange();
+    fetch(`${API_BASE_URL}/Airports`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Response:", data);
+        setAirportsData(data);
+      })
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
+  const hStyle = { color: "#FF385C" };
 
   return (
     <>
       <div className="facilities-section">
         <div className="intro-text">
-          
-          <h1> 
-            <select name="" id="airports-select">
-              <option value="">Aeropuerto Internacional de Jardines del Rey</option>
-              <option value="">Aeropuerto Internacional José Martí</option>
-              <option value="">Aeropuerto Internacional Ignacio Agramonte</option>
-              <option value="">Aeropuerto Gustavo Rizo</option>
-              <option value="">Aeropuerto Carlos Manuel de Céspedes</option>
-              <option value="">Aeropuerto de Pinar del Río</option>
-              <option value="">Aeropuerto de Sancti Spíritus</option>
-            </select> | <span style={hStyle}>Facilities</span></h1>
+          <h1>
+            <select
+              name=""
+              id="airports-select"
+              ref={selectRef}
+              style={{ width: selectWidth }}
+              onChange={handleSelectChange}
+            >
+              <option selected disabled value="">
+                Choose an airport
+              </option>
+              {airportsData.map(
+                (airport) =>
+                  !airport.isDeleted && (
+                    <option key={airport.id} value={airport.id}>
+                      Airport {airport.name}
+                    </option>
+                  )
+              )}
+            </select>{" "}
+            | <span style={hStyle}>Facilities</span>
+          </h1>
           <p>
             {" "}
             Hey there! 🛫 Are you ready to explore all the awesome amenities our
@@ -42,41 +104,16 @@ export const Facilities = () => {
         </div>
 
         <div className="facilities-container">
-          <FacilityCard
-            name="Breadway"
-            type="Cafeteria"
-            url={Breadway}
-          />
-          <FacilityCard
-            name="AMXWorkshop"
-            type="Workshop"
-            url={AMXWorkshop}
-          />
-          <FacilityCard
-            name="Tascon"
-            type="Clothing store"
-            url={Tascon}
-          />
-          <FacilityCard
-            name="ArtesaniaDominicana"
-            type="Gift shop"
-            url={ArtesaniaDominicana}
-          />
-          <FacilityCard
-            name="CambioExchange"
-            type="Currency exchange office"
-            url={CambioExchange}
-          />
-          <FacilityCard 
-            name="Ryu" 
-            type="Sushi Bar" 
-            url={Ryu} 
-          />
-          <FacilityCard
-            name="Tagliatella"
-            type="Restaurant"
-            url={Tagliatella}
-          />
+          {facilitiesData.map(
+            (facility) =>
+              !facility.isDeleted && (
+                <FacilityCard
+                  name={facility.name}
+                  type={facility.address}
+                  url={facility.imgUrl}
+                />
+              )
+          )}
         </div>
       </div>
     </>
