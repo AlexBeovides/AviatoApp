b3a44b9af80c5ba753e452aced47deedd571e4da
arielgg46 <lenin46ariel@gmail.com>
2024-04-02 00:20:13 -0500

    Added Stats page

--- src/pages/Stats.tsx
+++ src/pages/Stats.tsx
@@ -1,59 +1,38 @@
 import "../styles/Stats.scss";
-import { API_BASE_URL } from "../config";
-import { useEffect, useState } from "react";
-
-type Stat = {
-  description: string;
-  sqlQuery: string;
-};
+import React from "react";
+import * as XLSX from "xlsx";
+import { saveAs } from "file-saver";
 
 export const Stats = () => {
-  const token = localStorage.getItem("token");
+  const data = [
+    { name: "John", email: "john@example.com", age: 28 },
+    { name: "Jane", email: "jane@example.com", age: 32 },
+    // ... more data
+  ];
 
-  useEffect(() => {
-    fetch(`${API_BASE_URL}/Planes/MyPlanes`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Response:", data);
-      })
-      .catch((error) => console.error("Error:", error));
-  }, []);
+  const exportToExcel = (index: number) => {
+    const worksheet = XLSX.utils.json_to_sheet(data);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
 
-  const stats: Stat[] = [
-    {
-      description:
-        "Obtener los nombres y la posición geográfica de los aeropuertos que brinden servicio de reparación a las naves.",
-      sqlQuery: "",
-    },
-    {
-      description:
-        "Obtener la cantidad de reparaciones capitales que se han realizado en cada aeropuerto.",
-      sqlQuery: "",
-    },
-    {
-      description:
-        "Por tipo de cliente, obtener los nombres y el tipo de los clientes del aeropuerto internacional José Martí que han arribado a la misma en sus propias naves como capitanes.",
-      sqlQuery: "",
-    },
-    {
-      description:
-        "Obtener los nombres de los aeropuertos y la cantidad de servicios que brinda cada uno de ellos, para aquéllos que hayan recibido el menor número de naves después del año 2010.",
-      sqlQuery: "",
-    },
-    {
-      description:
-        "Obtener el monto promedio por cada uno de los servicios de reparación del aeropuerto internacional José Martí que han sido ineficientes en el último año transcurrido y cuya valoración por los clientes tenga un promedio menor a 5 puntos.",
-      sqlQuery: "",
-    },
-    {
-      description:
-        "Eliminar apropiadamente los servicios de un aeropuerto que hayan sido ineficientes.",
-      sqlQuery: "",
-    },
+    // Buffer to store the generated Excel file
+    const excelBuffer = XLSX.write(workbook, {
+      bookType: "xlsx",
+      type: "array",
+    });
+    const blob = new Blob([excelBuffer], {
+      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8",
+    });
+
+    saveAs(blob, "Query_" + index + ".xlsx");
+  };
+
+  const stats: string[] = [
+    "Obtener los nombres y la posición geográfica de los aeropuertos que brinden servicio de reparación a las naves.",
+    "Obtener la cantidad de reparaciones capitales que se han realizado en cada aeropuerto.",
+    "Por tipo de cliente, obtener los nombres y el tipo de los clientes del aeropuerto internacional José Martí que han arribado a la misma en sus propias naves como capitanes.",
+    "Obtener los nombres de los aeropuertos y la cantidad de servicios que brinda cada uno de ellos, para aquéllos que hayan recibido el menor número de naves después del año 2010.",
+    "Obtener el monto promedio por cada uno de los servicios de reparación del aeropuerto internacional José Martí que han sido ineficientes en el último año transcurrido y cuya valoración por los clientes tenga un promedio menor a 5 puntos.",
   ];
 
   return (
@@ -61,14 +40,16 @@ export const Stats = () => {
       <div className="stats-container">
         <h1 className="page-header">Stats:</h1>
         <ol>
-          {stats.map((stat) => (
-            <div>
+          {stats.map((stat, index) => (
+            <div key={index}>
               <li>
-                {stat.description}
+                {stat}
                 <div className="buttons-container">
-                  <button className="my-button query-button">Query</button>
-                  <button className="my-button export-button disabled-button">
-                    Export
+                  <button
+                    className="my-button query-button"
+                    onClick={() => exportToExcel(index + 1)}
+                  >
+                    Query
                   </button>
                 </div>
               </li>
