4f4acbc4d0b8aeff16f27c1b27103e639ea270d9
arielgg46 <lenin46ariel@gmail.com>
2024-04-01 19:29:25 -0500

    Updated Stats page

--- src/pages/Stats.tsx
+++ src/pages/Stats.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 
 type Stat = {
   description: string;
-  sqlRequest: string;
+  sqlQuery: string;
 };
 
 export const Stats = () => {
@@ -27,32 +27,32 @@ export const Stats = () => {
     {
       description:
         "Obtener los nombres y la posición geográfica de los aeropuertos que brinden servicio de reparación a las naves.",
-      sqlRequest: "",
+      sqlQuery: "",
     },
     {
       description:
         "Obtener la cantidad de reparaciones capitales que se han realizado en cada aeropuerto.",
-      sqlRequest: "",
+      sqlQuery: "",
     },
     {
       description:
         "Por tipo de cliente, obtener los nombres y el tipo de los clientes del aeropuerto internacional José Martí que han arribado a la misma en sus propias naves como capitanes.",
-      sqlRequest: "",
+      sqlQuery: "",
     },
     {
       description:
         "Obtener los nombres de los aeropuertos y la cantidad de servicios que brinda cada uno de ellos, para aquéllos que hayan recibido el menor número de naves después del año 2010.",
-      sqlRequest: "",
+      sqlQuery: "",
     },
     {
       description:
         "Obtener el monto promedio por cada uno de los servicios de reparación del aeropuerto internacional José Martí que han sido ineficientes en el último año transcurrido y cuya valoración por los clientes tenga un promedio menor a 5 puntos.",
-      sqlRequest: "",
+      sqlQuery: "",
     },
     {
       description:
         "Eliminar apropiadamente los servicios de un aeropuerto que hayan sido ineficientes.",
-      sqlRequest: "asd",
+      sqlQuery: "",
     },
   ];
 
@@ -62,12 +62,17 @@ export const Stats = () => {
         <h1 className="page-header">Stats:</h1>
         <ol>
           {stats.map((stat) => (
-            <li>
-              <div>
-                <p>{stat.description}</p>
-                <p>{stat.sqlRequest}</p>
-              </div>
-            </li>
+            <div>
+              <li>
+                {stat.description}
+                <div className="buttons-container">
+                  <button className="my-button query-button">Query</button>
+                  <button className="my-button export-button disabled-button">
+                    Export
+                  </button>
+                </div>
+              </li>
+            </div>
           ))}
         </ol>
       </div>
